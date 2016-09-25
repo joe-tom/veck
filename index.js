1df27a9d258672575af10bb16e81a2415516a82b
Joseph Thomas <joey6978@gmail.com>
2016-09-24 21:30:38 -0700

    Added constructor forObject and Arrays as well as a magnitude calculator

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
+function Vecky (vector) {
+  if (!(this instanceof Vecky)) return new Vecky(vector)
 
-
-function Vecky(vector) {
-  var vectorArray = []
-
+  this.vector = []
   switch (typeof vector) {
     case 'number':
 
-    break;
+      break
     case 'string':
-
-    break;
-    case 'object':
-      if (vector instanceof Array) {
+      var units = vector.replace(/ /g, '').split(/(\+|\-)(?=.)/g).filter((i) => (i))
+      units.sort(function (a, b) {
+        return (a < b ? -1 : (a > b ? 1 : 0))
+      })
+      for (var i = 0; i < units.length; i++) {
 
       }
-    break;
+      break
+    case 'object':
+      if (vector instanceof Array) {
+        this.vector = vector
+      } else {
+        Object.keys(vector).sort().forEach(function (key) {
+          this.vector[key] = vector[key]
+        })
+      }
+      break
   }
+
+  this.dimension = vectorArray.length
+}
+
+
+Vecky.prototype.magnitude = function () {
+  var sum = 0
+  for (var i = 0, ii = this.dimension; i < ii; i++) {
+    sum += this.vectorArray[i] * this.vectorArray[i]
+  }
+
+  return Math.sqrt(sum)
+}
+
+
+Vecky.prototype.valueOf = function () {
+  return this.magnitude()
 }
