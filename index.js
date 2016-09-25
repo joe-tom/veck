acb9187e5a7d6bdf600f37232dda7298acad0578
Joseph Thomas <joey6978@gmail.com>
2016-09-24 18:51:59 -0700

    Added constructor

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function Vecky(vector) {
+  var vectorArray = []
+
+  switch (typeof vector) {
+    case 'number':
+
+    break;
+    case 'string':
+
+    break;
+    case 'object':
+      if (vector instanceof Array) {
+
+      }
+    break;
+  }
+}
